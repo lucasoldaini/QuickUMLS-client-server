95afb810938ef6365479a1570a986ab0339e2325
Luca Soldaini <luca@soldaini.net>
2016-08-17 15:37:48 -0400

    using default pickle version protocol

--- quickumls_client.py
+++ quickumls_client.py
@@ -6,8 +6,6 @@ except ImportError:
     from .QuickUMLS.quickumls import QuickUMLS
 
 def get_quickumls_client(host='localhost', port=4645):
-    client = MinimalClient(
-        QuickUMLS, host=host, port=port,
-        buffersize=4096, pickle_protocol=2)
+    client = MinimalClient(QuickUMLS, host=host, port=port, buffersize=4096)
     return client
 
