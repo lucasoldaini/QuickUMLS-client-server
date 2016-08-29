e128430998d02a491f92c18ff73135057ef261c7
Luca Soldaini <luca@soldaini.net>
2016-08-29 13:23:04 -0400

    updated to reflect differences in MinimalServer and  QuickUMLS

--- quickumls_client.py
+++ quickumls_client.py
@@ -5,7 +5,7 @@ except ImportError:
     from .MinimalServer import MinimalClient
     from .QuickUMLS.quickumls import QuickUMLS
 
+
 def get_quickumls_client(host='localhost', port=4645):
     client = MinimalClient(QuickUMLS, host=host, port=port, buffersize=4096)
     return client
-
--- quickumls_server.py
+++ quickumls_server.py
@@ -2,15 +2,17 @@ from QuickUMLS.quickumls import QuickUMLS
 from argparse import ArgumentParser
 from MinimalServer import run_server
 
+
 def run_quickumls_server(opts):
     matcher = QuickUMLS(
         quickumls_fp=opts.quickumls_fp,
         threshold=opts.threshold,
         similarity_name=opts.similarity_name,
-        window=opts.window)
+        window=opts.window,
+        min_match_length=opts.min_match_length
+    )
 
-    run_server(matcher, host=opts.host, port=opts.port,
-               pickle_protocol=2, buffersize=4096)
+    run_server(matcher, host=opts.host, port=opts.port, buffersize=4096)
 
 if __name__ == '__main__':
     ap = ArgumentParser()
@@ -20,8 +22,6 @@ if __name__ == '__main__':
     ap.add_argument('-t', '--threshold', default=0.8, type=float)
     ap.add_argument('-s', '--similarity_name', default='jaccard')
     ap.add_argument('-w', '--window', default=5, type=int)
+    ap.add_argument('-l', '--min-match-length', default=3, type=int)
     opts = ap.parse_args()
     run_quickumls_server(opts)
-
-
-
